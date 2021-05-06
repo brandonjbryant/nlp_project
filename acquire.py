e1fe8de9d3ac639e9a9be56201b827b19ebbaac3
“Brandon <brandon.j.bryant1@gmail.com>
2021-05-06 17:23:40 -0500

    132 repos listed

--- acquire.py
+++ acquire.py
@@ -24,6 +24,185 @@ REPOS = [
     "gocodeup/codeup-setup-script",
     "gocodeup/movies-application",
     "torvalds/linux",
+    "abuanwar072/Flutter-Responsive-Admin-Panel-or-Dashboard",
+    "pallupz/covid-vaccine-booking",
+    "goharbor/harbor",
+    "public-apis/public-apis",
+    "jwasham/coding-interview-university",
+    "DIGITALCRIMINAL/OnlyFans",
+    "facebookincubator/cinder",
+    "vinta/awesome-python",
+    "jlevy/the-art-of-command-line",
+    "dogecoin/dogecoin",
+    "phamdinhkhanh/vnquant",
+    "audacity/audacity",
+    "Uniswap/uniswap-v3-periphery",
+    "swar/Swar-Chia-Plot-Manager",
+    "flutter/flutter",
+    "streamich/react-use",
+    "dynamicwebpaige/thinking-in-data",
+    "forem/forem",
+    "Azure/counterfit",
+    "Uniswap/uniswap-v3-core",
+    "ionic-team/ionic-framework",
+    "thedevdojo/wave",
+    "devsuperior/sds3",
+    "angular/angular-cli",
+    "benawad/dogehouse",
+    "jtleek/datasharing",
+    "rdpeng/ProgrammingAssignment2",
+    "octocat/Spoon-Knife",
+    "tensorflow/tensorflow",
+    "SmartThingsCommunity/SmartThingsPublic",
+    "twbs/bootstrap",
+    "github/gitignore",
+    "rdpeng/ExData_Plotting1",
+    "Pierian-Data/Complete-Python-3-Bootcamp",
+    "nightscout/cgm-remote-monitor",
+    "jwasham/coding-interview-university",
+    "opencv/opencv",
+    "tensorflow/models",
+    "EbookFoundation/free-programming-books",
+    "eugenp/tutorials",
+    "CyC2018/CS-Notes",
+    "jackfrued/Python-100-Days",
+    "rdpeng/RepData_PeerAssessment1",
+    "firstcontributions/first-contributions",
+    "Snailclimb/JavaGuide",
+    "facebook/react",
+    "spring-projects/spring-boot",
+    "jlord/patchwork",
+    "barryclark/jekyll-now",
+    "DataScienceSpecialization/courses",
+    "spring-projects/spring-framework",
+    "TheAlgorithms/Python",
+    "vuejs/vue",
+    "bitcoin/bitcoin",
+    "kubernetes/kubernetes",
+    "ant-design/ant-design",
+    "mrdoob/three.js",
+    "getify/You-Dont-Know-JS",
+    "freeCodeCamp/freeCodeCamp",
+    "DefinitelyTyped/DefinitelyTyped",
+    "PanJiaChen/vue-element-admin",
+    "donnemartin/system-design-primer",
+    "apache/dubbo",
+    "justjavac/free-programming-books-zh_CN",
+    "udacity/frontend-nanodegree-resume",
+    "kamranahmedse/developer-roadmap",
+    "d3/d3",
+    "ohmyzsh/ohmyzsh",
+    "mui-org/material-ui",
+    "facebook/create-react-app",
+    "dotnet/AspNetCore.Docs",
+    "git/git",
+    "scikit-learn/scikit-learn",
+    "LarryMad/recipes",
+    "996icu/996.ICU",
+    "macrozheng/mall",
+    "airbnb/javascript",
+    "iluwatar/java-design-patterns",
+    "laravel/laravel",
+    "facebook/react-native",
+    "sindresorhus/awesome",
+    "ansible/ansible",
+    "slatedocs/slate",
+    "nodejs/node",
+    "jquery/jquery",
+    "elastic/elasticsearch",
+    "shadowsocks/shadowsocks"
+    "rails/rails",
+    "qmk/qmk_firmware",
+    "redis/redis",
+    "vinta/awesome-python",
+    "BVLC/caffe",
+    "microsoft/vscode",
+    "keras-team/keras",
+    "python/cpython",
+    "wesbos/JavaScript30",
+    "trekhleb/javascript-algorithms",
+    "google/it-cert-automation-practice",
+    "moby/moby",
+    "labuladong/fucking-algorithm",
+    "apache/echarts",
+    "flutter/flutter",
+    "CSSEGISandData/COVID-19",
+    "pjreddie/darknet",
+    "helm/charts",
+    "xingshaocheng/architect-awesome",
+    "github/docs",
+    "ColorlibHQ/AdminLTE",
+    "wakaleo/game-of-life",
+    "danielmiessler/SecLists",
+    "aymericdamien/TensorFlow-Examples",
+    "public-apis/public-apis",
+    "doocs/advanced-java",
+    "jenkins-docs/simple-java-maven-app",
+    "reduxjs/redux",
+    "MicrosoftDocs/mslearn-tailspin-spacegame-web",
+    "scm-ninja/starter-web",
+    "mmistakes/minimal-mistakes",
+    "pallets/flask",
+    "RedHatTraining/DO180-apps",
+    "scutan90/DeepLearning-500-questions",
+    "tastejs/todomvc",
+    "CoreyMSchafer/code_snippets",
+    "academicpages/academicpages.github.io",
+    "selfteaching/the-craft-of-selfteaching",
+    "ionic-team/ionic-framework",
+    "mdn/learning-area",
+    "netty/netty", 
+    "MicrosoftDocs/azure-docs",
+    "jakevdp/PythonDataScienceHandbook",
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ]
 
 headers = {"Authorization": f"token {github_token}", "User-Agent": github_username}
